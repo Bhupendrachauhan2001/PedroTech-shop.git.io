44b1ea01ad5c598b8a52b9e39d9ed45a257ca410
Bhupendra Chauhan <104031135+Bhupendrachauhan2001@users.noreply.github.com>
2023-03-02 08:53:49 -0800

    reactjs project

---
+++ precache-manifest.d7e660b5627e04bc1f6ffdee12e2da53.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "02260589b28bdec86775",
+    "url": "/static/css/main.a2b5dd16.chunk.css"
+  },
+  {
+    "revision": "02260589b28bdec86775",
+    "url": "/static/js/main.02260589.chunk.js"
+  },
+  {
+    "revision": "a55ae8da76b6c0c45aec",
+    "url": "/static/js/1.a55ae8da.chunk.js"
+  },
+  {
+    "revision": "9c697eae1f15525501e4",
+    "url": "/static/css/2.44df4891.chunk.css"
+  },
+  {
+    "revision": "9c697eae1f15525501e4",
+    "url": "/static/js/2.9c697eae.chunk.js"
+  },
+  {
+    "revision": "4a4fcd1f8f9790e96d57",
+    "url": "/static/js/runtime~main.4a4fcd1f.js"
+  },
+  {
+    "revision": "5bbed59fefa89250f78941a702d0c98f",
+    "url": "/static/media/1.5bbed59f.png"
+  },
+  {
+    "revision": "97ab95661ae5d6f5926fbf31025ae3ce",
+    "url": "/static/media/2.97ab9566.png"
+  },
+  {
+    "revision": "644f4281f1a79df6cb4088106f8816b7",
+    "url": "/static/media/3.644f4281.png"
+  },
+  {
+    "revision": "a6c4f63e82462bff1f324f6dc2481560",
+    "url": "/static/media/4.a6c4f63e.png"
+  },
+  {
+    "revision": "ded28d66394426adc2f9a99b6bf129d3",
+    "url": "/static/media/5.ded28d66.png"
+  },
+  {
+    "revision": "4bad6a1b2f7f4f418c8f4cf6dd6ac67a",
+    "url": "/static/media/6.4bad6a1b.png"
+  },
+  {
+    "revision": "3e4dc3c23d414cf566747d253918e17d",
+    "url": "/static/media/7.3e4dc3c2.png"
+  },
+  {
+    "revision": "f70aa67d2ef17cd45b882efa08f943cc",
+    "url": "/static/media/8.f70aa67d.png"
+  },
+  {
+    "revision": "78560ac27b90fe98baae975c86d6dbf9",
+    "url": "/index.html"
+  }
+];
